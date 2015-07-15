75962cef5f6567d6c1909737e8890cd977aa4668
Luke Zappia <luke.zappia@gmail.com>
2015-07-15 16:08:16 +1000

    Add normalisation functions

---
+++ R/normalise.R
@@ -0,0 +1,72 @@
+#' edgeR Normalise
+#'
+#' Normalise a DGEList object using edgeR
+#'
+#' @param dge DGEList object to normalise
+#'
+#' @return DGEList object with normalistion factors
+#'
+#' @export
+edgeRNorm <- function(dge) {
+
+    dge <- edgeR::calcNormFactors(dge)
+    dge <- edgeR::estimateCommonDisp(dge)
+    dge <- edgeR::estimateTagwiseDisp(dge)
+
+    return(dge)
+}
+
+
+#' DESeq Normalise
+#'
+#' Normalise a CountDataSet object using DESeq
+#'
+#' @param count.data CountDataSet object to normalise
+#'
+#' @return Normalised CountDataSet object
+#'
+#' @export
+deseqNorm <- function(count.data) {
+
+    count.data <- DESeq::estimateSizeFactors(count.data)
+    count.data <- DESeq::estimateDispersions(count.data)
+
+    return(count.data)
+}
+
+
+#' DESeq2 Normalise
+#'
+#' Normalise a DESeqDataSet using DESeq2
+#'
+#' @param count.data DESeqDataSet object to normalise
+#'
+#' @return Normalised DESeqDataSet object
+#'
+#' @export
+deseq2Norm <- function(count.data) {
+
+    count.data <- DESeq2::DESeq(count.data)
+
+    return(count.data)
+}
+
+#' voom Normalise
+#'
+#' Normalise a DGEList object using limma-voom
+#'
+#' @param dge DGEList object to normalise
+#'
+#' @return Normalised EList object
+#'
+#' @export
+voomNorm <- function(dge) {
+
+    groups <- dge$samples$group
+    design <- model.matrix(~ groups)
+
+    dge       <- edgeR::calcNormFactors(dge)
+    voom.data <- limma::voom(dge, design)
+
+    return(voom.data)
+}
