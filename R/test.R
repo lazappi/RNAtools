ee8005e2fbcc794e562b11a4856631adad46336c
Luke Zappia <luke.zappia@gmail.com>
2015-07-16 09:43:55 +1000

    Write deseq2Test function

--- R/test.R
+++ R/test.R
@@ -59,13 +59,18 @@ deseqTest <- function(count.data, group1, group2) {
 #' Test a normalised DESeqDataSet using DESeq2
 #'
 #' @param count.data Normalised DESeqDataSet object to normalise
+#' @param group1 First group to test, the reference or control
+#' @param group2 Second group to test, the treatment
 #'
-#' @return
+#' @return DESeqResults object containing test results
 #'
 #' @export
-deseq2Test <- function(count.data) {
+deseq2Test <- function(count.data, group1, group2) {
 
-    return()
+    results <- DESeq2::results(count.data, contrast = c("group", group1, group2))
+    results <- results[order(results$padj), ]
+
+    return(results)
 }
 
 #' voom Test
