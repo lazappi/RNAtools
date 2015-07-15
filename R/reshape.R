0f2d968d5e64ce34e63babdfec4973fa6430e239
Luke Zappia <luke.zappia@gmail.com>
2015-07-15 15:12:14 +1000

    Add functions for converting to DE objects

--- R/reshape.R
+++ R/reshape.R
@@ -20,7 +20,6 @@ lengthenMatrix <- function(data) {
     return(long)
 }
 
-
 #' Combine Matrices
 #'
 #' Combine a list of matrices with the same number of columns into a single long
@@ -47,3 +46,113 @@ combineMatrices <- function(data.list, lengthen = TRUE) {
     return(combined)
 }
 
+#' Counts to Objects
+#'
+#' Convert a count matrix to objects required by various differential expression
+#' testing packages
+#'
+#' @param data    Matrix of counts
+#' @param groups  Vector of groups for each sample column
+#' @param methods Vector of object types to convert to
+#' @param verbose Boolean, whether to print messages showing progress
+#'
+#' @return List of converted objects
+#'
+#' @export
+counts2Objects <- function(data, groups,
+                           methods = c("edgeR", "DESeq", "DESeq2", "voom"),
+                           verbose = TRUE) {
+
+    methods <- unique(methods)
+
+    objects <- list()
+
+    for (method in methods) {
+        switch(
+            method,
+
+            edgeR = {
+                if (verbose) {message("Creating object for edgeR...")}
+                objects$edgeR <- counts2edgeR(data, groups)
+            },
+
+            DESeq = {
+                if (verbose) message("Creating object for DESeq...")
+                objects$DESeq <- counts2DESeq(data, groups)
+            },
+
+            DESeq2 = {
+                if (verbose) message("Creating DESeq2 object...")
+                objects$DESeq2 <- counts2DESeq2(data, groups)
+            },
+
+            voom = {
+                if (verbose) {message("Creating voom object...")}
+                objects$voom <- counts2edgeR(data, groups)
+            },
+
+            stop(paste("Method", method, "not recognised. Allowed methods are:",
+                       "edgeR/DESeq/DESeq2/voom"))
+        )
+    }
+
+    # Sort list by method name
+    objects <- objects[order(names(objects))]
+
+    return(objects)
+}
+
+#' Counts to edgeR
+#'
+#' Convert a count matrix to an edgeR DGEList object
+#'
+#' @param data    Matrix of counts
+#' @param groups  Vector of groups for each sample column
+#'
+#' @return DGEList object containing counts
+#'
+#' @export
+counts2edgeR <- function(data, groups) {
+
+    dge <- edgeR::DGEList(data, group = groups)
+
+    return(dge)
+}
+
+#' Counts to DESeq
+#'
+#' Convert a count matrix to a DESeq CountDataSeq object
+#'
+#' @param data    Matrix of counts
+#' @param groups  Vector of groups for each sample column
+#'
+#' @return CountDataSet object containing counts
+#'
+#' @export
+counts2DESeq <- function(data, groups) {
+
+    count.data <- DESeq::newCountDataSet(data, conditions = groups)
+
+    return(count.data)
+}
+
+#' Counts to DESeq2
+#'
+#' Convert a count matrix to a DESeq2 DESeqDataSet object
+#'
+#' @param data    Matrix of counts
+#' @param groups  Vector of groups for each sample column
+#'
+#' @return CountDataSet object containing counts
+#'
+#' @export
+counts2DESeq2 <- function(data, groups) {
+
+    groups <- data.frame(group = groups)
+
+    count.data <- DESeq2::DESeqDataSetFromMatrix(data,
+                                                 colData = groups,
+                                                 design = ~ group)
+
+    return(count.data)
+}
