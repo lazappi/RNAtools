59151766068cd70792f067c241f4f378279b1fc2
Luke Zappia <luke.zappia@gmail.com>
2015-07-16 16:20:06 +1000

    Add listVolcano function

--- R/volcano_plot.R
+++ R/volcano_plot.R
@@ -1,3 +1,60 @@
+#' List Volcano
+#'
+#' Plot volcano plots from a list of differential expression results
+#'
+#' @param data.list List of results to plot
+#'
+#' @return List of ggplot2 object containing volcano plots
+#'
+#' @importFrom magrittr "%>%"
+#'
+#' @export
+listVolcano <- function(data.list) {
+
+    plots <- list()
+    regular.data.list <- list()
+
+    for (name in names(data.list)) {
+
+        data <- data.list[[name]]
+
+        regular.data <- regulariseResults(data, name)
+
+        gg <- volcanoPlot(regular.data, method = "regular")
+
+        regular.data.list[[name]] <- regular.data
+        plots[[name]] <- gg
+    }
+
+    gg <- regular.data.list %>%
+          combineMatrices(lengthen = FALSE) %>%
+          dplyr::mutate(logSig = -log(Significance)) %>%
+          dplyr::mutate(absFC = abs(FoldChange)) %>%
+          dplyr::mutate(absFC = replace(absFC, !is.finite(absFC),
+                                        max(absFC[is.finite(absFC)]))) %>%
+          ggplot2::ggplot(ggplot2::aes(x      = FoldChange, y = logSig,
+                                       colour = logSig, alpha = absFC)) +
+          ggplot2::annotate("rect",
+                            xmin = -Inf, xmax = Inf,
+                            ymin = -Inf, ymax = -log(0.05),
+                            alpha = 0.2, fill = "blue", size = 0) +
+          ggplot2::annotate("rect",
+                            xmin = -2,   xmax = 2,
+                            ymin = -Inf, ymax = Inf,
+                            alpha = 0.2, fill = "red", size = 0) +
+          ggplot2::geom_point(size = 4) +
+          ggplot2::facet_wrap(~ matrix) +
+          ggplot2::scale_color_gradient(low = "#3f007d", high = "#ef3b2c") +
+          ggplot2::xlab("log Fold Change") +
+          ggplot2::ylab("-log Significance") +
+          ggplot2::theme(legend.position = "none")
+
+
+    plots[["combined"]] <- gg
+
+    return(plots)
+}
+
 #' Volcano Plot
 #'
 #' Produce a volcano plot from differential expression results
