4fe33a6fa6a27459bd1b4f17b3975d624b64183e
lazappi <lazappi@users.noreply.github.com>
2015-07-18 16:36:03 +1000

    Fix deseq2test

--- R/test.R
+++ R/test.R
@@ -120,6 +120,7 @@ deseq2Test <- function(count.data, group1, group2, filter) {
     if (filter) {
         n.start <- nrow(count.data)
         filtered <- HTSFilter(count.data, plot = FALSE)
+        count.data <- filtered$filteredData
 
         message(paste("HTSFilter threshold:", filtered$s,
                       "Genes Filtered:", n.start - nrow(count.data)))
