8eb62f4f858794eb270df7be7dfdfe5968795ff6
Luke Zappia <luke.zappia@gmail.com>
2015-07-20 11:41:33 +1000

    Add listPlotPvals function

--- R/p_values.R
+++ R/p_values.R
@@ -1,3 +1,48 @@
+#' List p-value plots
+#'
+#' Plot hitograms of p-values from a list of differential expression results
+#'
+#' @param data.list List of results to plot
+#'
+#' @return List of ggplot2 objects containing p-value histograms
+#'
+#' @importFrom magrittr "%>%"
+#'
+#' @export
+listPlotPvals <- function(data.list) {
+
+    plots <- list()
+    regular.data.list <- list()
+
+    for (name in names(data.list)) {
+
+        data <- data.list[[name]]
+
+        regular.data <- regulariseResults(data, name)
+
+        gg <- plotPvals(regular.data, method = "regular")
+
+        regular.data.list[[name]] <- regular.data
+        plots[[name]] <- gg
+    }
+
+    gg <- regular.data.list %>%
+          combineMatrices(lengthen = FALSE) %>%
+          ggplot2::ggplot(ggplot2::aes(x = pValue)) +
+          ggplot2::annotate("rect",
+                            xmin = -Inf, xmax = 0.05,
+                            ymin = -Inf, ymax = Inf,
+                            alpha = 0.2, fill = "red", size = 0) +
+          ggplot2::geom_histogram(binwidth = 0.025) +
+          ggplot2::facet_wrap(~ matrix) +
+          ggplot2::xlab("p-value") +
+          ggplot2::ylab("Counts")
+
+    plots[["combined"]] <- gg
+
+    return(plots)
+}
+
 #' Plot p-values
 #'
 #' Produce a histogram of p-values from differential expression results
@@ -26,6 +71,10 @@ plotPvals <- function(results,
     }
 
     gg <- ggplot2::ggplot(results, ggplot2::aes(x = pValue)) +
+          ggplot2::annotate("rect",
+                            xmin = -Inf, xmax = 0.05,
+                            ymin = -Inf, ymax = Inf,
+                            alpha = 0.2, fill = "red", size = 0) +
           ggplot2::geom_histogram(binwidth = 0.025) +
           ggplot2::xlab("p-value") +
           ggplot2::ylab("Counts")
